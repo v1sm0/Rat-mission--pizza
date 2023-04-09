4044bf965e59ffad0cd4d4b38ec994eaa8ae62e1
DiegoOpazoD <diegoopazod@gmail.com>
2023-04-08 23:35:49 -0400

    Se agrego un menu basico y la coneccion para el boton Exit

--- InterfazDeConexión.gd
+++ InterfazDeConexión.gd
@@ -1,11 +1,15 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_host_pressed():
 	pass # Replace with function body.
 
+func _on_join_pressed():
+	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_options_pressed():
+	pass # Replace with function body.
+
+func _on_exit_pressed():
+	get_tree().quit()
+	
