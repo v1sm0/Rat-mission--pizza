f911e132812274c810ec2acf64f1758f5341f556
Vicente Soto <vicentesoto@ug.uchile.cl>
2023-04-14 13:31:16 -0400

    lobby avance

--- scripts/lobby.gd
+++ scripts/lobby.gd
@@ -1,12 +1,20 @@
 extends MarginContainer
 
-const MAX_Player = 2
+const MAX_PLAYER = 2
 const PORT = 3002
 
 @onready var nameInput = %NameInput
 @onready var ipInput = %IPInput
 @onready var host = %Host
 @onready var join = %Join
+
+@onready var lobby = %Lobby
+@onready var readyScreen = %ReadyScreen
+
+@onready var cancel = $PanelContainer/MarginContainer/ReadyScreen/HBoxContainer/Cancel
+@onready var go = $PanelContainer/MarginContainer/ReadyScreen/HBoxContainer/Go
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# nameInput.text
@@ -17,13 +25,24 @@ func _ready():
 	multiplayer.peer_connected.connect(_on_peer_connected)
 	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
 	multiplayer.server_disconnected.connect(_on_server_disconnected)
+	lobby.show()
+	readyScreen.hide()
 	pass
 
 func _on_host_pressed() -> void:
 	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(PORT, MAX_PLAYER)
+	multiplayer.multiplayer_peer = peer
+	lobby.hide()
+	readyScreen.show()
 	print("host")
 	
 func _on_join_pressed() -> void:
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(ipInput.text, PORT)
+	multiplayer.multiplayer_peer = peer	
+	lobby.hide()
+	readyScreen.show()
 	print("join")
 
 func _on_connected_to_server() -> void:
@@ -32,10 +51,12 @@ func _on_connected_to_server() -> void:
 func _on_connection_failed() -> void:
 	pass
 
-func _on_peer_connected() -> void:
+func _on_peer_connected(id: int) -> void:
+	Debug.print("peer_connected %d" % id)
 	pass
 	
-func _on_peer_disconnected() -> void:
+func _on_peer_disconnected(id: int) -> void:
+	Debug.print("peer_disconnected %d" % id)	
 	pass
 	
 func _on_server_disconnected() -> void:
