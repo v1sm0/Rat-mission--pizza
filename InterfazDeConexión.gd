7ee781b7339f3c9e91d07f43128d2a14a66518c8
DiegoOpazoD <diegoopazod@gmail.com>
2023-04-16 13:23:43 -0400

    Se reemplazo la font de los menus y se agrego el codigo para que los objetos del fondo del menu funcionen

--- InterfazDeConexión.gd
+++ InterfazDeConexión.gd
@@ -1,5 +1,13 @@
 extends Control
 
+func  _on_play_pressed():
+	get_tree().change_scene_to_file("res://playMenu.tscn")
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+func _on_exit_pressed():
+	get_tree().quit()
 
 func _on_host_pressed():
 	pass # Replace with function body.
@@ -7,9 +15,5 @@ func _on_host_pressed():
 func _on_join_pressed():
 	pass # Replace with function body.
 
-func _on_options_pressed():
-	pass # Replace with function body.
-
-func _on_exit_pressed():
-	get_tree().quit()
-	
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://menu.tscn")
--- menu_backgound.gd
+++ menu_backgound.gd
@@ -1,11 +1,31 @@
 extends Node2D
 
-
+@onready var obj1 = $obj1 
+@onready var obj2 = $obj2
+ 
+var down_dir = Vector2(0,200)
 # Called when the node enters the scene tree for the first time.
-func _ready(): 
+func _ready():
+	obj1.global_position.y = randi() % 100 + 10
+	obj1.global_position.x =  randi() % 1000 + 10
+	obj1.set_axis_velocity(down_dir)
+	obj2.add_constant_torque(randi()%500)
+	
+	obj2.global_position.y = randi() % 100 + 10
+	obj2.global_position.x =  randi() % 1000 + 10
+	obj2.set_axis_velocity(down_dir)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if obj1.global_position.y > 700:
+		obj1.global_position.y = -(randi() % 300 + 10)
+		obj1.global_position.x =  randi() % 1000 + 10
+		obj1.set_axis_velocity(down_dir)
+		
+	if obj2.global_position.y > 700:
+		obj2.global_position.y = -(randi() % 300 + 10)
+		obj2.global_position.x =  randi() % 1000 + 10
+		obj2.set_axis_velocity(down_dir)
 	pass
