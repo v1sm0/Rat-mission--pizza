ec2a56e273abc800158e1f818e619d2be2a1be9d
Vicente Soto <vicentesoto@ug.uchile.cl>
2023-04-21 12:59:39 -0400

    cambios en la conexión
    Se hacen ultimos cambios para la conexión online. Queda pendiente conectar esto a un nivel inicial

--- scenes/lobby.gd
+++ scenes/lobby.gd
@@ -1,13 +1,15 @@
 extends MarginContainer
 
-const MAX_PLAYER = 2
+const MAX_PLAYERS = 2
 const PORT = 5409
 
+@onready var info = $PanelContainer/MarginContainer/Lobby/Info
 
 @onready var nameInput = %NameInput
 @onready var ipInput = %IPInput
 @onready var host = %Host
 @onready var join = %Join	
+@onready var players = %Players
 
 @onready var lobby = %Lobby
 @onready var readyScreen = %ReadyScreen
@@ -15,6 +17,7 @@ const PORT = 5409
 @onready var cancel = $PanelContainer/MarginContainer/ReadyScreen/HBoxContainer/Cancel
 @onready var go = $PanelContainer/MarginContainer/ReadyScreen/HBoxContainer/Go
 
+var status = { 1 : false }
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,34 +32,68 @@ func _ready():
 	lobby.show()
 	readyScreen.hide()
 	nameInput.text = OS.get_environment("USERNAME")
+	go.pressed.connect(_on_go_pressed)
+	info.hide()
+
+
+func _on_go_pressed() -> void:
+	rpc("player_ready")
+	_paint_ready(multiplayer.get_unique_id())
+
+func _paint_ready(id: int) -> void:
+	for child in players.get_children():
+		if child.name == str(id):
+			child.modulate = Color.GREEN_YELLOW
 
 func _on_host_pressed() -> void:
 	Debug.print("host")
 	var peer = ENetMultiplayerPeer.new()
-	peer.create_server(PORT, MAX_PLAYER)
+	var err = peer.create_server(PORT, MAX_PLAYERS)
+	print(err)
 	multiplayer.multiplayer_peer = peer
 	lobby.hide()
+	_add_player(nameInput.text, multiplayer.get_unique_id())
 	readyScreen.show()
-	print("host")
 	
 func _on_join_pressed() -> void:
 	Debug.print("join")
 	var peer = ENetMultiplayerPeer.new()
-	peer.create_client(ipInput.text, PORT)
-	multiplayer.multiplayer_peer = peer	
+	var err = peer.create_client(ipInput.text, PORT)
+	print(err)
+	multiplayer.multiplayer_peer = peer
 	lobby.hide()
+	_add_player(nameInput.text, multiplayer.get_unique_id())
 	readyScreen.show()
-	print("join")
+
+func _add_player(name: String, id: int):
+	var label = Label.new()
+	label.name = str(id)
+	label.text = name
+	players.add_child(label)
+	Game.players.append(id)
+
+
+@rpc("any_peer", "reliable")
+func send_info(info: Dictionary) -> void:
+	var name = info.name
+	var id = multiplayer.get_remote_sender_id()
+	_add_player(name, id)
+
 
 func _on_connected_to_server() -> void:
+	Debug.print("connected_to_server")
 	pass
 	
 func _on_connection_failed() -> void:
+	Debug.print("connection_failed")
+	
 	pass
 
 func _on_peer_connected(id: int) -> void:
 	Debug.print("peer_connected %d" % id)
-	pass
+	rpc_id(id, "send_info", { "name": nameInput.text })
+	if multiplayer.is_server():
+		status[id] = false
 	
 func _on_peer_disconnected(id: int) -> void:
 	Debug.print("peer_disconnected %d" % id)	
