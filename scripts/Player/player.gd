cd764f1e8e4ba23e5707a810c1e51f96df6fd71d
Nadia <nadiadecar@ug.uchile.cl>
2023-04-23 23:49:32 -0400

    Se arregla un problema en el movimiento del personaje de un lado a otro y se hace la conexión del menú a su script correspondiente

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -5,6 +5,9 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const ACCELERATION = 1000
 
+
+var FACING_RIGHT = false
+
 @onready var animation_tree = $AnimationTree
 @onready var character = $"."
 
@@ -31,8 +34,11 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var move_input = Input.get_axis("Left", "Right")
 	if Input.is_action_just_pressed("Left") or Input.is_action_just_pressed("Right"):
-		if move_input>0.5:
-			character.scale.x = move_input
+		if (FACING_RIGHT and move_input<0) or ((not FACING_RIGHT) and move_input>0):
+			character.scale.x = -1 * abs(character.scale.x)
+			FACING_RIGHT = not FACING_RIGHT
+			
+			
 	velocity.x = move_toward(velocity.x, move_input * SPEED, ACCELERATION * delta)
 	
 	
