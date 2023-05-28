f9cff0db407bddd3ce1bef23998390e9f91c7864
DiegoOpazoD <diegoopazod@gmail.com>
2023-05-28 17:36:40 -0400

    ahora el boton funciona bien

--- scripts/Niveles/botones.gd
+++ scripts/Niveles/botones.gd
@@ -2,13 +2,12 @@ extends Node2D
 
 
 func _ready():
-	$AnimationPlayer.play("ButtonDown")
+	$AnimationPlayer.play("ButtonUp")
 
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
 		$AnimationPlayer.play("ButtonDown")
-		print("hola")
 
 
 func _on_area_2d_body_exited(body):
