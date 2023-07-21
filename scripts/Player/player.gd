16909c3778408afe37f6d520c9759aaf2e86e85f
nadiadecar <nadiadecar@ug.uchile.cl>
2023-07-21 03:51:00 -0400

    Se mejoran las cajas

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -5,42 +5,55 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -450.0
 const ACCELERATION = 1000
 
-var JUMPING = false
-var FACING_RIGHT = true
-
-signal empujando(quien)
-signal quieto(quien)
-signal chocan_rata(quien)
-signal ya_no_chocan_rata(quien)
+var force = 0
+var pushing_forces = 0: 
+	set(value) : 
+		Debug.print('se sssteta')
+		pushing_forces = value
+		if multiplayer.is_server(): 
+			Debug.print('es server')
+			Debug.print(value)
+			update_pushing_forces.rpc_id(get_multiplayer_authority(), pushing_forces)
+var jumping = false
+var pushers = {}
+var collider = null
 
 @onready var animation_tree = $AnimationTree
 @onready var animation_player = $AnimationPlayer
-@onready var character = $pivot
-@onready var colision = $CollisionShape2D
+@onready var pivot = $pivot
+@onready var collision = $CollisionShape2D
+@onready var ray = $pivot/ray
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var move_input = 0
 
-
 func init(id):
 	set_multiplayer_authority(id)
 	name = str(id)
 
-func _ready():
-	Debug.print(name)
-	
+func _ready():	
 	set_multiplayer_authority(name.to_int())
-	
 	animation_tree.active = true
 	animation_player.play('IDLE')
 	print('Se correo la animación')
 
 
 func _physics_process(delta):
+	if multiplayer.is_server():
+		if ray.is_colliding():
+			collider = ray.get_collider() as Node
+			if collider.has_method("being_pushed"):
+				collider.being_pushed(self, force + pushing_forces)
+		else: 
+			if collider != null: 
+				if collider.has_method("stoped_being_pushed"):
+					collider.stoped_being_pushed(self)
+					collider = null
+			
 	if is_multiplayer_authority():
-		# Add the gravity.
+		# Add the gravity
 		if not is_on_floor():
 			velocity.y += gravity * delta
 
@@ -48,42 +61,31 @@ func _physics_process(delta):
 		if Input.is_action_just_pressed("Jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			animation_player.play("Saltar")
-			JUMPING = true
+			jumping = true
 			
 			
 		move_input = Input.get_axis("Left", "Right")
+		force = move_input
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
-		if Input.is_action_pressed("Left") and FACING_RIGHT:
-			character.scale.x = -1
-			colision.position = - colision.position
-			FACING_RIGHT = false
-			
-		elif Input.is_action_pressed("Right") and not FACING_RIGHT:
-			character.scale.x = 1
-			colision.position = - colision.position
-			FACING_RIGHT = true
-			
-				
-		if Input.is_action_just_pressed("Left") or Input.is_action_just_pressed("Right") or Input.is_action_pressed("Left") or Input.is_action_pressed("Right"):
-			emit_signal("empujando", self, move_input)
-			
-			
-		velocity.x = move_toward(velocity.x, move_input * SPEED, ACCELERATION * delta)
+		if move_input != 0:
+			pivot.scale.x = sign(move_input)
+		
+		velocity.x = move_toward(velocity.x, clamp(move_input+pushing_forces,-1,1) * SPEED, ACCELERATION * delta)
+#		Debug.print(pushing_forces)
 		
 		if abs(velocity.x) < 0.5: 
 			animation_player.play("IDLE")
-			emit_signal("quieto", self)			
 			
-		elif (not JUMPING) and (abs(velocity.x) > 0.5):
+		elif (not jumping) and (abs(velocity.x) > 0.5):
 			animation_player.play("Caminar")
 		
 		if abs(velocity.y)<0.2:
-			JUMPING = false
+			jumping = false
 
 		
 		move_and_slide()
-		
+
 		rpc("send_data", global_position, velocity, move_input)
 		
 			
@@ -92,20 +94,40 @@ func _physics_process(delta):
 func send_data(pos: Vector2, vel: Vector2, mi: float) -> void:
 	global_position = lerp(global_position, pos, 0.5)
 	velocity = lerp(velocity, vel, 0.5)
-	move_input = mi
+	
+	if(mi != 0):
+		pivot.scale.x = sign(mi) 
+	force = mi
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var move_input = Input.get_axis("Left", "Right")
-	if Input.is_action_just_pressed("Left") or Input.is_action_just_pressed("Right"):
-		if (FACING_RIGHT and move_input<0) or ((not FACING_RIGHT) and move_input>0):
-			character.scale.x = -1 * abs(character.scale.x)
-			FACING_RIGHT = not FACING_RIGHT
-
-func _on_area_rata_body_entered(body):
-	print('chocan a la rata')
-	emit_signal("chocan_rata",body)
+@rpc("reliable","any_peer")
+func update_pushing_forces(new_pushing_forces: int):
+	Debug.print('Se updeitea la pushing')
+	pushing_forces = new_pushing_forces
 
 
-func _on_area_rata_body_exited(body):
-	emit_signal("ya_no_chocan_rata", body)
+func being_pushed(pusher: Node, forces: int):
+	if pusher == collider and forces == force:
+		pass
+	else: 
+		if not pushers.has(pusher):
+			Debug.print('nuevo pusher')
+			pushers[pusher] = forces
+			pushing_forces += forces
+			change_pushing_force()
+		else: 
+			if pushers[pusher] != forces:
+				Debug.print('fuerza distinta')
+				pushing_forces += (forces - pushers[pusher])
+				pushers[pusher] = forces
+				change_pushing_force()
+
+func change_pushing_force():
+	if ray.is_colliding():
+		var collider := ray.get_collider() as Node
+		if collider.has_method("being_pushed"):
+			collider.being_pushed(self, force + pushing_forces)
+
+func stoped_being_pushed(pusher: Node):
+	if pushers.has(pusher):
+		pushing_forces -= pushers[pusher]
+		pushers.erase(pusher)
--- scripts/debug.gd
+++ scripts/debug.gd
@@ -1,9 +1,12 @@
 extends Node
 
+@onready var canvas_layer  = CanvasLayer.new()
 @onready var container = VBoxContainer.new()
 
 func _ready() -> void:
-	add_child(container)
+	add_child(canvas_layer)
+	canvas_layer.layer = 100
+	canvas_layer.add_child(container)
 
 func print(message: Variant) -> void:
 	if not multiplayer.multiplayer_peer is OfflineMultiplayerPeer:
@@ -12,6 +15,8 @@ func print(message: Variant) -> void:
 		print(message)
 	var label = Label.new()
 	label.text = str(message)
+	label.set("theme_override_constants/outline_size", 2)
+	label.set("theme_override_colors/font_outline_color", Color.BLACK)
 	container.add_child(label)
 	container.move_child(label, 0)
 	await get_tree().create_timer(2).timeout
